fef685f31f95091ec2a9418afcf2312654ecb20a
Iclementem <iclementem@outlook.es>
2022-08-04 07:52:03 +0200

    Initial Commit

---
+++ iOS_Xcode_Navigation/iOS_Xcode_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Xcode_NavigationApp.swift
+//  iOS_Xcode_Navigation
+//
+//  Created by Iván Clemente Moreno on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Xcode_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
